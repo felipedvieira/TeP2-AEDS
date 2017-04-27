c2a99a1a66117609c061ad5d92e931da110c2124
Felipe Duarte Vieira <lipao23@hotmail.com>
2017-04-27 20:09:39 -0300

    comit2

--- parenteses.cpp
+++ parenteses.cpp
@@ -2,4 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-d
+int main(){
+	char c;
+	while(cin >> ("%c", &c) !=EOF){
+		int valido = 1;
+		int Abre = 0, Fecha = 0;
+		while(valido == 1){
+			if(c =='(')
+				Abre++;
+			else if (c ==')') 
+				Fecha++;
+			if (Fecha > Abre)
+				valido = 0;
+			c =getchar();
+			if(c == '\n'){
+				break;
+			}
+
+		}
+		if(Abre == Fecha && valido) 
+			cout << "Correct" << endl;
+		else 
+			cout << "Incorrect" << endl;
+	}
+}
